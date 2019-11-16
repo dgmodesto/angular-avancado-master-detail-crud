a50784dac0d68e9233a10c26d4b44e5643871d43
dgmodesto <douglasgomesmodesto@gmail.com>
2019-11-16 12:26:55 -0300

    improving category form template

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../shared/category.service';
+import { Category } from '../shared/category.model';
 
 @Component({
   selector: 'app-category-list',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-list.component.css']
 })
 export class CategoryListComponent implements OnInit {
+  categories: Category[] = [];
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit() {
+    this.categoryService.getAll().subscribe(
+      categories => {
+        this.categories = categories;
+      },
+      error => alert('Erro ao carregar a lista')
+    );
   }
 
+  deleteCategory(category) {
+    const mustDelete = confirm('Deseja excluir este item?');
+
+    if (mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        () =>
+          (this.categories = this.categories.filter(
+            element => element != category
+          )),
+        () => alert('Erro ao tentar excluir!')
+      );
+    }
+  }
 }
