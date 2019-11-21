ce62cd7cc66cbe60a9813308455040e43c2600e9
dgmodesto <douglasgomesmodesto@gmail.com>
2019-11-21 12:08:28 -0300

    improving entry form component

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -5,9 +5,16 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { EntriesRoutingModule } from './entries-routing.module';
 import { EntryListComponent } from './entry-list/entry-list.component';
 import { EntryFormComponent } from './entry-form/entry-form.component';
-
+import { CalendarModule } from 'primeng/calendar';
+import { IMaskModule } from 'angular-imask';
 @NgModule({
   declarations: [EntryListComponent, EntryFormComponent],
-  imports: [CommonModule, EntriesRoutingModule, ReactiveFormsModule]
+  imports: [
+    CommonModule,
+    EntriesRoutingModule,
+    ReactiveFormsModule,
+    CalendarModule,
+    IMaskModule
+  ]
 })
 export class EntriesModule {}
