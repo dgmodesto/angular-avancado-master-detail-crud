9ee4d975e114c18265cec7e7c700e199fe1cd10d
dgmodesto <douglasgomesmodesto@gmail.com>
2019-12-01 18:52:10 -0300

    improving page header component

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PageHeaderComponent implements OnInit {
   // tslint:disable: no-input-rename
   @Input('page-title') pageTitle: string;
+  @Input('show-button') showButton = true;
   @Input('button-class') buttonClass: string;
   @Input('button-text') buttonText: string;
   @Input('button-link') buttonLink: string;
